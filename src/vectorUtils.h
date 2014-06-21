e4ae4e74cbcfd28a812fa4c28939b29868398d68
daid <daid303@gmail.com>
2014-06-21 18:24:04 +0200

    Add general function for calculating difference between angles

--- src/vectorUtils.h
+++ src/vectorUtils.h
@@ -71,6 +71,16 @@ namespace sf
         return atan2(v.x, -v.y) / M_PI * 180;
     }
 
+    /* Return the difference between angle_a and angle_b within a range of -180 and 180 degrees */
+    template <typename T>
+    T angleDifference(const T& angle_a, const T& angle_b)
+    {
+        T ret = (angle_b - angle_a);
+        while(ret > 180) ret -= 360;
+        while(ret < -180) ret += 360;
+        return ret;
+    }
+
     template <typename T>
     Vector2<T> rotateVector(const Vector2<T>& v, const T& angle)
     {
