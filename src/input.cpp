057ab9efcf4b579c35df500fc9a3ba8676a573e0
daid <daid303@gmail.com>
2014-08-14 08:25:30 +0200

    Support mouse button release detection. Add support for touch screens. Add support to swap the X/Y of the mouse for the strange touch screens we bought.

--- src/input.cpp
+++ src/input.cpp
@@ -2,6 +2,8 @@
 #include "engine.h"
 
 P<WindowManager> InputHandler::windowManager;
+bool InputHandler::touch_screen = false;
+bool InputHandler::swap_xy = false;
 
 sf::Vector2f InputHandler::mousePos;
 int InputHandler::mouse_wheel_delta;
@@ -10,8 +12,10 @@ bool InputHandler::keyboard_button_down[sf::Keyboard::KeyCount];
 
 bool InputHandler::mouseButtonDown[sf::Mouse::ButtonCount];
 bool InputHandler::mouseButtonPressed[sf::Mouse::ButtonCount];
+bool InputHandler::mouseButtonReleased[sf::Mouse::ButtonCount];
 bool InputHandler::keyboardButtonDown[sf::Keyboard::KeyCount];
 bool InputHandler::keyboardButtonPressed[sf::Keyboard::KeyCount];
+bool InputHandler::keyboardButtonReleased[sf::Keyboard::KeyCount];
 string InputHandler::keyboard_text_entry;
 
 void InputHandler::initialize()
@@ -25,7 +29,16 @@ void InputHandler::update()
     if (!windowManager)
         windowManager = engine->getObject("windowManager");
 
-    mousePos = sf::Vector2f(sf::Mouse::getPosition() - windowManager->window.getPosition());
+    mousePos = sf::Vector2f(sf::Mouse::getPosition());
+    if (swap_xy)
+    {
+        sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+        float x = mousePos.y * desktop.width / desktop.height;
+        float y = mousePos.x * desktop.height / desktop.width;
+        mousePos.x = x;
+        mousePos.y = y;
+    }
+    mousePos = mousePos - sf::Vector2f(windowManager->window.getPosition());
     mousePos.x *= float(windowManager->virtualSize.x) / float(windowManager->window.getSize().x);
     mousePos.y *= float(windowManager->virtualSize.y) / float(windowManager->window.getSize().y);
     for(unsigned int n=0; n<sf::Mouse::ButtonCount; n++)
@@ -33,6 +46,7 @@ void InputHandler::update()
         //bool down = sf::Mouse::isButtonPressed(sf::Mouse::Button(n)) && windowManager->hasFocus();    //
         bool down = mouse_button_down[n];
         mouseButtonPressed[n] = (!mouseButtonDown[n] && down);
+        mouseButtonReleased[n] = (mouseButtonDown[n] && !down);
         mouseButtonDown[n] = down;
     }
     
@@ -40,6 +54,20 @@ void InputHandler::update()
     {
         bool down = keyboard_button_down[n];
         keyboardButtonPressed[n] = (!keyboardButtonDown[n] && down);
+        keyboardButtonReleased[n] = (keyboardButtonDown[n] && !down);
         keyboardButtonDown[n] = down;
+    }
+    
+    if (touch_screen)
+    {
+        bool any_button_down = false;
+        for(unsigned int n=0; n<sf::Mouse::ButtonCount; n++)
+            if (mouseButtonDown[n] || mouseButtonReleased[n])
+                any_button_down = true;
+        if (!any_button_down)
+        {
+            mousePos.x = -1;
+            mousePos.y = -1;
+        }
     }
 }
--- src/input.h
+++ src/input.h
@@ -16,21 +16,28 @@ class InputHandler
 
     static bool mouseButtonDown[sf::Mouse::ButtonCount];
     static bool mouseButtonPressed[sf::Mouse::ButtonCount];
+    static bool mouseButtonReleased[sf::Mouse::ButtonCount];
     static bool keyboardButtonDown[sf::Keyboard::KeyCount];
     static bool keyboardButtonPressed[sf::Keyboard::KeyCount];
+    static bool keyboardButtonReleased[sf::Keyboard::KeyCount];
     static string keyboard_text_entry;
 
     static void initialize();
 public:
+    static bool touch_screen;
+    static bool swap_xy;
+    
     static void update();
 
     static bool keyboardIsDown(sf::Keyboard::Key key) { return keyboardButtonDown[key]; }
     static bool keyboardIsPressed(sf::Keyboard::Key key) { return keyboardButtonPressed[key]; }
+    static bool keyboardIsReleased(sf::Keyboard::Key key) { return keyboardButtonReleased[key]; }
     static string getKeyboardTextEntry() { return keyboard_text_entry; }
 
     static sf::Vector2f getMousePos() { return mousePos; }
     static bool mouseIsDown(sf::Mouse::Button button) { return mouseButtonDown[button]; }
     static bool mouseIsPressed(sf::Mouse::Button button) { return mouseButtonPressed[button]; }
+    static bool mouseIsReleased(sf::Mouse::Button button) { return mouseButtonReleased[button]; }
     static int getMouseWheelDelta() { return mouse_wheel_delta; }
 
     friend class Engine;
