12ea4db75d9912e7eb6d2b72ab39ac49f011d0fb
daid <daid303@gmail.com>
2015-07-29 19:45:57 +0200

    Use new C++11 random number generators.

---
+++ src/random.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
+#include <random>
+
+#include "random.h"
+
+static std::mt19937_64 random_engine;
+
+void initRandom()
+{
+    random_engine.seed(time(NULL));
+}
+
+float random(float fmin, float fmax)
+{
+    return std::uniform_real_distribution<>(fmin, fmax)(random_engine);
+}
+
+int irandom(int imin, int imax)
+{
+    return std::uniform_int_distribution<>(imin, imax)(random_engine);
+}
--- src/random.h
+++ src/random.h
@@ -1,23 +1,10 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
-#include <stdlib.h>
-#include <time.h>
-#include <stdint.h>
+void initRandom();
 
-static inline void initRandom()
-{
-    srand(time(NULL));
-}
+float random(float fmin, float fmax);
 
-static inline float random(float fmin, float fmax)
-{
-    return (float(rand()) / float(RAND_MAX)) * (fmax - fmin) + fmin;
-}
-
-static inline int irandom(int imin, int imax)
-{
-    return (int64_t(rand()) * int64_t(imax - imin + 1) / (int64_t(RAND_MAX) + 1)) + imin;
-}
+int irandom(int imin, int imax);
 
 #endif//RANDOM_H
