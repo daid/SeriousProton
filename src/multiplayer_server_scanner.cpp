efd95a5d05599f2a5f4d7ba20b68dcaf7ef0b3dc
daid <daid303@gmail.com>
2015-10-09 08:27:12 +0200

    Update the server scanner code to support the master server

--- src/multiplayer_server_scanner.cpp
+++ src/multiplayer_server_scanner.cpp
@@ -1,42 +1,44 @@
 #include "multiplayer_server_scanner.h"
 
-ServerScanner::ServerScanner(int versionNumber, int serverPort)
-: serverPort(serverPort), versionNumber(versionNumber)
+ServerScanner::ServerScanner(int version_number, int server_port)
+: server_port(server_port), version_number(version_number), master_server_scan_thread(&ServerScanner::masterServerScanThread, this)
 {
-    int portNr = serverPort + 1;
-    while(socket.bind(portNr) != sf::UdpSocket::Done)
-        portNr++;
+    int port_nr = server_port + 1;
+    while(socket.bind(port_nr) != sf::UdpSocket::Done)
+        port_nr++;
     
     socket.setBlocking(false);
-    broadcastDelay = 0.0;
+    broadcast_clock.restart();
+}
+
+ServerScanner::~ServerScanner()
+{
+    destroy();
+    master_server_scan_thread.wait();
 }
 
 void ServerScanner::update(float gameDelta)
 {
-    //Calculate our own delta, as we want wall-time delta, the gameDelta can be modified by the current game speed (could even be 0 on pause)
-    float delta = updateTimeClock.getElapsedTime().asSeconds();
-    updateTimeClock.restart();
-
-    broadcastDelay -= delta;
-    if (broadcastDelay < 0.0)
+    if (broadcast_clock.getElapsedTime().asSeconds() > BroadcastTimeout)
     {
         sf::Packet sendPacket;
-        sendPacket << multiplayerVerficationNumber << "ServerQuery" << int32_t(versionNumber);
-        UDPbroadcastPacket(socket, sendPacket, serverPort);
-        broadcastDelay += 2.0;
+        sendPacket << multiplayerVerficationNumber << "ServerQuery" << int32_t(version_number);
+        UDPbroadcastPacket(socket, sendPacket, server_port);
+        broadcast_clock.restart();
     }
     
-    for(unsigned int n=0; n<serverList.size(); n++)
+    server_list_mutex.lock();
+    for(unsigned int n=0; n<server_list.size(); n++)
     {
-        serverList[n].timeout += delta;
-        if (serverList[n].timeout > serverTimeout)
+        if (server_list[n].timeout_clock.getElapsedTime().asSeconds() > ServerTimeout)
         {
             if (removedServerCallback)
-                removedServerCallback(serverList[n].address);
-            serverList.erase(serverList.begin() + n);
+                removedServerCallback(server_list[n].address);
+            server_list.erase(server_list.begin() + n);
             n--;
         }
     }
+    server_list_mutex.unlock();
 
     sf::IpAddress recvAddress;
     unsigned short recvPort;
@@ -46,32 +48,38 @@ void ServerScanner::update(float gameDelta)
         int32_t verification, versionNr;
         string name;
         recvPacket >> verification >> versionNr >> name;
-        if (verification == multiplayerVerficationNumber && (versionNr == versionNumber || versionNr == 0 || versionNumber == 0))
+        if (verification == multiplayerVerficationNumber && (versionNr == version_number || versionNr == 0 || version_number == 0))
         {
-            bool found = false;
-            for(unsigned int n=0; n<serverList.size(); n++)
-            {
-                if (serverList[n].address == recvAddress)
-                {
-                    serverList[n].timeout = 0.0;
-                    serverList[n].name = name;
-                    found = true;
-                }
-            }
-            if (!found)
-            {
-                LOG(INFO) << "ServerScanner::New server: " << recvAddress.toString() << " " << recvPort << " " << name;
-                ServerInfo si;
-                si.address = recvAddress;
-                si.timeout = 0.0;
-                si.name = name;
-                serverList.push_back(si);
-                
-                if (newServerCallback)
-                    newServerCallback(recvAddress, name);
-            }
+            updateServerEntry(recvAddress, recvPort, name);
         }
     }
+}
+
+void ServerScanner::updateServerEntry(sf::IpAddress address, int port, string name)
+{
+    sf::Lock lock(server_list_mutex);
+    
+    for(unsigned int n=0; n<server_list.size(); n++)
+    {
+        if (server_list[n].address == address)
+        {
+            server_list[n].port = port;
+            server_list[n].name = name;
+            server_list[n].timeout_clock.restart();
+            return;
+        }
+    }
+
+    LOG(INFO) << "ServerScanner::New server: " << address.toString() << " " << port << " " << name;
+    ServerInfo si;
+    si.address = address;
+    si.port = port;
+    si.name = name;
+    si.timeout_clock.restart();
+    server_list.push_back(si);
+    
+    if (newServerCallback)
+        newServerCallback(address, name);
 }
 
 void ServerScanner::addCallbacks(std::function<void(sf::IpAddress, string)> newServerCallback, std::function<void(sf::IpAddress)> removedServerCallback)
@@ -81,6 +89,68 @@ void ServerScanner::addCallbacks(std::function<void(sf::IpAddress, string)> newS
 }
 
 std::vector<ServerScanner::ServerInfo> ServerScanner::getServerList()
+{   
+    std::vector<ServerScanner::ServerInfo> ret;
+    server_list_mutex.lock();
+    ret = server_list;
+    server_list_mutex.unlock();
+    return ret;
+}
+
+void ServerScanner::scanMasterServer(string url)
 {
-    return serverList;
+    master_server_url = url;
+    master_server_scan_thread.launch();
+}
+
+void ServerScanner::masterServerScanThread()
+{
+    if (!master_server_url.startswith("http://"))
+    {
+        LOG(ERROR) << "Master server URL does not start with \"http://\"";
+        return;
+    }
+    string hostname = master_server_url.substr(7);
+    int path_start = hostname.find("/");
+    if (path_start < 0)
+    {
+        LOG(ERROR) << "Master server URL has no uri after hostname";
+        return;
+    }
+    string uri = hostname.substr(path_start + 1);
+    hostname = hostname.substr(0, path_start);
+    
+    LOG(INFO) << "Reading servers from master server";
+    
+    sf::Http http(hostname);
+    while(!isDestroyed())
+    {
+        sf::Http::Request request(uri, sf::Http::Request::Get);
+        sf::Http::Response response = http.sendRequest(request, sf::seconds(10.0f));
+        
+        if (response.getStatus() != sf::Http::Response::Ok)
+        {
+            LOG(WARNING) << "Failed to query master server (" << response.getStatus() << ")";
+        }
+        for(string line : string(response.getBody()).split("\n"))
+        {
+            LOG(DEBUG) << "Line:" << line;
+            std::vector<string> parts = line.split(":", 3);
+            if (parts.size() == 4)
+            {
+                sf::IpAddress address(parts[0]);
+                int port = parts[1].toInt();
+                int version = parts[2].toInt();
+                string name = parts[3];
+                
+                if (version == version_number || version == 0 || version_number == 0)
+                {
+                    updateServerEntry(address, port, name);
+                }
+            }
+        }
+        
+        for(int n=0;n<10 && !isDestroyed();n++)
+            sf::sleep(sf::seconds(1.0f));
+    }
 }
--- src/multiplayer_server_scanner.h
+++ src/multiplayer_server_scanner.h
@@ -7,33 +7,47 @@
 //Class to find all servers that have the correct version number. Creates a big nice list.
 class ServerScanner : public Updatable
 {
-    sf::Clock updateTimeClock;
-    int serverPort;
+    int server_port;
     sf::UdpSocket socket;
-    float broadcastDelay;
+    sf::Clock broadcast_clock;
 
 public:
     struct ServerInfo
     {
         sf::IpAddress address;
-        float timeout;
+        int port;
         string name;
+
+        sf::Clock timeout_clock;
     };
 private:
-    std::vector<struct ServerInfo> serverList;
-    int versionNumber;
-    constexpr static float serverTimeout = 30.0f;
+    std::vector<struct ServerInfo> server_list;
+    int version_number;
+    constexpr static float BroadcastTimeout = 2.0f;
+    constexpr static float ServerTimeout = 30.0f;
+    
+    string master_server_url;
+    sf::Mutex server_list_mutex;
+    sf::Thread master_server_scan_thread;
     
     std::function<void(sf::IpAddress, string)> newServerCallback;
     std::function<void(sf::IpAddress)> removedServerCallback;
 public:
 
-    ServerScanner(int versionNumber, int serverPort = defaultServerPort);
+    ServerScanner(int version_number, int server_port = defaultServerPort);
+    virtual ~ServerScanner();
 
     virtual void update(float delta);
     void addCallbacks(std::function<void(sf::IpAddress, string)> newServerCallback, std::function<void(sf::IpAddress)> removedServerCallback);
+    
+    void scanMasterServer(string url);
 
     std::vector<ServerInfo> getServerList();
+
+private:
+    void masterServerScanThread();
+    
+    void updateServerEntry(sf::IpAddress address, int port, string name);
 };
 
 #endif//MULTIPLAYER_SERVER_SCANER_H
