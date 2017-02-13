6c24bba32fa8045904c53e98379c48d4abb4d6c4
Kyle Robertze <krobbertze@gmail.com>
2017-02-13 15:32:06 +0200

    added shared library build

---
+++ cmake/UseSeriousProton.cmake
@@ -0,0 +1,9 @@
+#                                               -*- cmake -*-
+#
+#  UseSeriousProton.cmake
+#
+
+add_definitions     ( ${SERIOUSPROTON_DEFINITIONS} )
+include_directories ( ${SERIOUSPROTON_INCLUDE_DIRS} )
+link_directories    ( ${SERIOUSPROTON_LIBRARY_DIRS} )
+
