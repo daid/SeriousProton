829a84ffe294dbe5d343ff91c988f2fda0de26b9
gcask <53709079+gcask@users.noreply.github.com>
2021-04-17 16:04:02 -0400

    Update gameEntity.cpp
    * Unreferenced parameters

--- src/gameEntity.cpp
+++ src/gameEntity.cpp
@@ -2,6 +2,6 @@
 
 PVector<GameEntity> entityList;
 GameEntity::~GameEntity(){}
-void GameEntity::update(float delta){}
-void GameEntity::render(sf::RenderTarget& window) {}
-bool GameEntity::takeDamage(sf::Vector2f position, int damageType, int damage_amount) { return false; }
+void GameEntity::update(float /*delta*/){}
+void GameEntity::render(sf::RenderTarget& /*window*/) {}
+bool GameEntity::takeDamage(sf::Vector2f /*position*/, int /*damageType*/, int /*damage_amount*/) { return false; }
