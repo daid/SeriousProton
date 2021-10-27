a658581d8013af9797c185ffc2473981a2f1e816
gcask <53709079+gcask@users.noreply.github.com>
2021-10-27 16:35:39 -0400

    convert indentation to spaces.

--- src/graphics/ktx2texture.h
+++ src/graphics/ktx2texture.h
@@ -10,32 +10,32 @@
 #include "resources.h"
 
 namespace sp {
-	class BasicTexture;
+    class BasicTexture;
 
-	class KTX2Texture
-	{
-	public:
-		bool loadFromStream(P<ResourceStream> stream);
+    class KTX2Texture
+    {
+    public:
+        bool loadFromStream(P<ResourceStream> stream);
 
-		std::optional<Image> toImage(uint32_t mip_level = 0);
-		std::unique_ptr<BasicTexture> toTexture(uint32_t mip_level = 0);
-		std::vector<uint8_t> toNative(uint32_t mip_level = 0);
+        std::optional<Image> toImage(uint32_t mip_level = 0);
+        std::unique_ptr<BasicTexture> toTexture(uint32_t mip_level = 0);
+        std::vector<uint8_t> toNative(uint32_t mip_level = 0);
 
-		glm::ivec2 getSize(uint32_t mip_level = 0) const;
-		uint32_t getNativeFormat() const;
-		uint32_t getMipCount() const;
+        glm::ivec2 getSize(uint32_t mip_level = 0) const;
+        uint32_t getNativeFormat() const;
+        uint32_t getMipCount() const;
 
-		KTX2Texture();
-		~KTX2Texture();
-		
-		// Transferrable, non-copyable.
-		KTX2Texture(KTX2Texture&&);
-		KTX2Texture& operator =(KTX2Texture&&);
-		KTX2Texture(const KTX2Texture&) = delete;
-		KTX2Texture& operator =(const KTX2Texture&) = delete;
-	private:
-		class Details;
-		std::unique_ptr<Details> details;
-	};
+        KTX2Texture();
+        ~KTX2Texture();
+
+        // Transferrable, non-copyable.
+        KTX2Texture(KTX2Texture&&);
+        KTX2Texture& operator =(KTX2Texture&&);
+        KTX2Texture(const KTX2Texture&) = delete;
+        KTX2Texture& operator =(const KTX2Texture&) = delete;
+    private:
+        class Details;
+        std::unique_ptr<Details> details;
+    };
 }
 #endif // SP_GRAPHICS_KTX2TEXTURE_H
