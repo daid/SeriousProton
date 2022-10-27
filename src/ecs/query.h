88837dd090bd316bf60ba4aaa659c8b467ee4561
Daid <daid303@gmail.com>
2022-10-27 09:28:38 +0200

    Add optional components to a query.

--- src/ecs/query.h
+++ src/ecs/query.h
@@ -6,6 +6,19 @@
 
 namespace sp::ecs {
 
+template<typename T> struct optional {};
+
+template<typename T> struct optional_info {
+    using base_type = T;
+    using ref_type = T&;
+    static constexpr bool value = false;
+};
+template<typename T> struct optional_info<optional<T>> {
+    using base_type = T;
+    using ref_type = T*;
+    static constexpr bool value = true;
+};
+
 template<class PRIMARY, class... T> class Query {
 public:
     class Iterator {
@@ -15,12 +28,24 @@ public:
 
         bool operator!=(const Iterator& other) const { return iterator != other.iterator; }
         void operator++() { ++iterator; checkForSkip(); }
-        std::tuple<Entity, PRIMARY&, T&...> operator*() {
+        std::tuple<Entity, PRIMARY&, typename optional_info<T>::ref_type...> operator*() {
             auto [index, primary] = *iterator;
-            return {Entity::fromIndex(index), primary, ComponentStorage<T>::storage.sparseset.get(index)...};
+            return {Entity::fromIndex(index), primary, getComponent<T>(index)...};
         }
     
     private:
+        template<typename T2> typename optional_info<T2>::ref_type getComponent(uint32_t index)
+        {
+            if constexpr (optional_info<T2>::value)
+            {
+                if (!ComponentStorage<typename optional_info<T2>::base_type>::storage.sparseset.has(index))
+                    return nullptr;
+                return &ComponentStorage<typename optional_info<T2>::base_type>::storage.sparseset.get(index);
+            } else {
+                return ComponentStorage<T2>::storage.sparseset.get(index);
+            }
+        }
+
         void checkForSkip() {
             if (iterator.atEnd()) return;
             if constexpr (sizeof...(T) > 0) {
@@ -31,8 +56,10 @@ public:
         }
         template<typename T2, typename... ARGS> bool checkIfHasAll() {
             auto index = (*iterator).first;
-            if (!ComponentStorage<T2>::storage.sparseset.has(index))
-                return false;
+            if constexpr (!optional_info<T2>::value) {
+                if (!ComponentStorage<T2>::storage.sparseset.has(index))
+                    return false;
+            }
             if constexpr (sizeof...(ARGS) > 0)
                 return checkIfHasAll<ARGS...>();
             return true;
