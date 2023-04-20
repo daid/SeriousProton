daid/SeriousProton

commit 9825f2d0659a6eb074562800d897088a10e4e3ff

minor fixes in network (#184)
* in Debug, my compiler asserted because of the sort returning integers (which is wrong, since in C++ the sort predicates uses a bool).
* Using `vector` for the addr, since `string` have special handling around nullbytes.

src/io/network/address.h
@@ -30,7 +30,7 @@ private:
     
         int family; //One of the AF_* macros, currently only AF_INET or AF_INET6
         string human_readable;
-        std::string addr;    //We abuse the std::string as a data buffer, it allows for easy memory management.
+        std::vector<uint8_t> addr;
     };
 
     Address(std::list<AddrInfo>&& addr_info);

src/io/network/address.cpp
@@ -149,15 +149,15 @@ Address Address::getLocalAddress()
     addr_info.sort([](const AddrInfo& a, const AddrInfo& b)
     {
         if (a.family == b.family)
-            return memcmp(a.addr.data(), b.addr.data(), std::min(a.addr.size(), b.addr.size()));
-        return a.family - b.family;
+            return memcmp(a.addr.data(), b.addr.data(), std::min(a.addr.size(), b.addr.size())) <= 0;
+        return (a.family - b.family) <= 0;
     });
 
     return Address(std::move(addr_info));
 }
 
 Address::AddrInfo::AddrInfo(int family, const string& human_readable, const void* addr, size_t addrlen)
-: family(family), human_readable(human_readable), addr(static_cast<const char*>(addr), addrlen)
+: family(family), human_readable(human_readable), addr(static_cast<const uint8_t *>(addr), static_cast<const uint8_t*>(addr) + addrlen)
 {
 }
 

src/io/network/tcpSocket.cpp
@@ -188,11 +188,11 @@ bool TcpSocket::connect(const Address& host, int port)
         handle = ::socket(addr_info.family, SOCK_STREAM, 0);
         if (handle == INVALID_SOCKET)
             return false;
-        if (addr_info.family == AF_INET && sizeof(struct sockaddr_in) == addr_info.addr.length())
+        if (addr_info.family == AF_INET && sizeof(struct sockaddr_in) == addr_info.addr.size())
         {
             struct sockaddr_in server_addr;
             memset(&server_addr, 0, sizeof(server_addr));
-            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.length());
+            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.size());
             server_addr.sin_port = htons(port);
             if (::connect(handle, reinterpret_cast<const sockaddr*>(&server_addr), sizeof(server_addr)) == 0)
             {
@@ -200,11 +200,11 @@ bool TcpSocket::connect(const Address& host, int port)
                 return true;
             }
         }
-        if (addr_info.family == AF_INET6 && sizeof(struct sockaddr_in6) == addr_info.addr.length())
+        if (addr_info.family == AF_INET6 && sizeof(struct sockaddr_in6) == addr_info.addr.size())
         {
             struct sockaddr_in6 server_addr;
             memset(&server_addr, 0, sizeof(server_addr));
-            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.length());
+            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.size());
             server_addr.sin6_port = htons(port);
             if (::connect(handle, reinterpret_cast<const sockaddr*>(&server_addr), sizeof(server_addr)) == 0)
             {

src/io/network/udpSocket.cpp
@@ -111,10 +111,10 @@ bool UdpSocket::joinMulticast(int group_nr)
     {
         for(const auto& addr_info : Address::getLocalAddress().addr_info)
         {
-            if (addr_info.family == AF_INET && !socket_is_ipv6 && addr_info.addr.length() == sizeof(struct sockaddr_in))
+            if (addr_info.family == AF_INET && !socket_is_ipv6 && addr_info.addr.size() == sizeof(struct sockaddr_in))
             {
                 struct sockaddr_in server_addr;
-                memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.length());
+                memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.size());
                 
                 struct ip_mreq mreq;
                 mreq.imr_multiaddr.s_addr = htonl((239 << 24) | (192 << 16) | (group_nr));
@@ -166,9 +166,9 @@ bool UdpSocket::send(const void* data, size_t size, const Address& address, int 
         memset(&server_addr, 0, sizeof(server_addr));
         for(auto& addr_info : address.addr_info)
         {
-            if (addr_info.family == AF_INET6 && addr_info.addr.length() == sizeof(server_addr))
+            if (addr_info.family == AF_INET6 && addr_info.addr.size() == sizeof(server_addr))
             {
-                memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.length());
+                memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.size());
                 is_set = true;
                 break;
             }
@@ -189,9 +189,9 @@ bool UdpSocket::send(const void* data, size_t size, const Address& address, int 
     memset(&server_addr, 0, sizeof(server_addr));
     for(auto& addr_info : address.addr_info)
     {
-        if (addr_info.family == AF_INET && addr_info.addr.length() == sizeof(server_addr))
+        if (addr_info.family == AF_INET && addr_info.addr.size() == sizeof(server_addr))
         {
-            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.length());
+            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.size());
             is_set = true;
             break;
         }
@@ -287,10 +287,10 @@ bool UdpSocket::sendMulticast(const void* data, size_t size, int group_nr, int p
     bool success = false;
     for(const auto& addr_info : Address::getLocalAddress().addr_info)
     {
-        if (addr_info.family == AF_INET && !socket_is_ipv6 && addr_info.addr.length() == sizeof(struct sockaddr_in))
+        if (addr_info.family == AF_INET && !socket_is_ipv6 && addr_info.addr.size() == sizeof(struct sockaddr_in))
         {
             struct sockaddr_in server_addr;
-            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.length());
+            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.size());
             
             ::setsockopt(handle, IPPROTO_IP, IP_MULTICAST_IF, reinterpret_cast<const char*>(&server_addr.sin_addr), sizeof(server_addr.sin_addr));
 
@@ -304,10 +304,10 @@ bool UdpSocket::sendMulticast(const void* data, size_t size, int group_nr, int p
             if (result == int(size))
                 success = true;
         }
-        if (addr_info.family == AF_INET6 && socket_is_ipv6 && addr_info.addr.length() == sizeof(struct sockaddr_in6))
+        if (addr_info.family == AF_INET6 && socket_is_ipv6 && addr_info.addr.size() == sizeof(struct sockaddr_in6))
         {
             struct sockaddr_in6 server_addr;
-            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.length());
+            memcpy(&server_addr, addr_info.addr.data(), addr_info.addr.size());
             
             ::setsockopt(handle, IPPROTO_IPV6, IPV6_MULTICAST_IF, reinterpret_cast<const char*>(&server_addr.sin6_addr), sizeof(server_addr.sin6_addr));
 

commit dc9dccf6c802b9c55ac4eccad424d2b1af9f4b93

io/network/address.h: add missing cstdint include
With 9825f2d0659a ("minor fixes in network (#184)") the type of addr
was changed from std::string to std::vector<uint8_t>, however the
header for uint8_t was not included.

Downstream Gentoo bug: https://bugs.gentoo.org/894738

Fixes: 9825f2d0659a ("minor fixes in network (#184)")

src/io/network/address.h
@@ -2,6 +2,7 @@
 #define SP2_IO_NETWORK_ADDRESS_H
 
 #include <stringImproved.h>
+#include <cstdint>
 #include <list>
 
 